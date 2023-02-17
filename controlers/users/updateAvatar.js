5f90ba18305c600ab0d7692e552423d33bd2450b
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2023-02-17 14:41:50 +0200

    final change

--- controlers/users/updateAvatar.js
+++ controlers/users/updateAvatar.js
@@ -1,13 +1,14 @@
 const path = require("path");
 const fs = require("fs/promises");
 const { User } = require("../../schemas/schemas");
+const { v4: uuidv4 } = require("uuid");
 
 const avatarDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
     const { path: tempUpload, originalname } = req.file;
     const { _id: id } = req.user;
-    const imageName = `${id}_${originalname}`;
+    const imageName = `${id}_${originalname}+${uuidv4()}`;
     try {
         const resultUpload = path.join(avatarDir, imageName);  
         await fs.rename(tempUpload, resultUpload); 
